0bed772d0522603016997a126b0b0d99dc09b57b
mojtaba nafez <mojtabanafez96@gmail.com>
2023-11-08 18:07:20 +0330

    update

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -250,6 +250,18 @@ def get_dataset(P, dataset, test_only=False, image_size=None, download=False, ev
         for i in range(len(anomaly_trainset)):
             anomaly_trainset.targets[i] = 1
         test_set = anomaly_testset
+        
+        def get_loader_unique_label(loader):
+            print('here is get_loader_unique_label')
+            try:
+                unique_labels = set()
+                for _, labels in loader:
+                    unique_labels.update(labels.tolist())
+                unique_labels = sorted(list(unique_labels))
+            except:
+                print("can not compute unique loader!")
+                unique_labels = []
+            return unique_labels
         print("-----------------")
         print("Unique labels(ood_test_loader):", get_loader_unique_label(ood_test_loader[ood]))
 

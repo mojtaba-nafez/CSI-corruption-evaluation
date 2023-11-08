dfbbb58484bc80024614bb56055ec1b55bb08f56
mojtaba nafez <mojtabanafez96@gmail.com>
2023-11-08 18:10:59 +0330

    update

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -263,7 +263,7 @@ def get_dataset(P, dataset, test_only=False, image_size=None, download=False, ev
                 unique_labels = []
             return unique_labels
         print("-----------------")
-        dd = DataLoader(ood_test_set, shuffle=False, batch_size=P.test_batch_size, **kwargs)
+        dd = DataLoader(test_set, shuffle=False, batch_size=64)
 
         print("Unique labels(ood_test_loader):", get_loader_unique_label(dd))
 
